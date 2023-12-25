652df1998c5e252b6e3856405a684ceaa03da925
wendellhernandez <wendellhbusiness@gmail.com>
2023-12-25 10:30:22 +0800

    added lights to header. Added glow to top of tree. and made tree bigger

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
-setInterval(function merryChristmass() {
-    let treeContainer = document.querySelector('.tree .container');
+let treeContainer = document.querySelector('.tree .container');
 
-    treeContainer.innerHTML = '<span class="red">';
+setInterval(() => {
+    treeContainer.innerHTML = '<span class="red"><span class="yellow"> O </span><br><span class="yellow"> O </span><span class="yellow"> O </span><br><span class="yellow"> O </span><span class="yellow"> O </span><span class="yellow"> O </span>';
     
     lights();
     
     treeContainer.innerHTML += '</span>';
     
-    treeContainer.innerHTML += '<div class="orange">mWm</div><div class="orange">mWm</div><br><span class="red">MERRY CHRISTMAS<br>and lots of <span id="CC">C</span><span id="OO">O</span><span id="DD">D</span><span id="EE">E</span> in 2023</span>';
-    
-    function lights(){
-        for(let i=0; i<24; i+=2){
-            treeContainer.innerHTML += '<br>';
-            for(let j=0; j<=i; j++){
-
-                    treeContainer.innerHTML += '<span class="rainbow"> * </span>';
-                
-            }
-        }
-    }
+    treeContainer.innerHTML += '<div class="orange">mWm</div><div class="orange">mWm</div><br><span class="red">and lots of <span class="rainbows">C</span><span class="rainbows">O</span><span class="rainbows">D</span><span class="rainbows">E</span> in 2023</span>';
     
     let rainbow = document.querySelectorAll('.rainbow');
-    let CC = document.querySelector('#CC');
-    let OO = document.querySelector('#OO');
-    let DD = document.querySelector('#DD');
-    let EE = document.querySelector('#EE');
+    let rainbows = document.querySelectorAll('.rainbows');
     
     for(let i=0; i<rainbow.length; i++){
-        rainbow[i].innerHTML = ' * ';
+        rainbow[i].innerHTML = ' O ';
         rainbowColor(rainbow[i]);
     }
-    
-    rainbowColor(CC);
-    rainbowColor(OO);
-    rainbowColor(DD);
-    rainbowColor(EE);
-    
-    function rainbowColor(element){
-        element.style['color'] = `rgb(${rand1to255()}, ${rand1to255()}, ${rand1to255()})`;
+
+    for(let i=0; i<rainbows.length; i++){
+        rainbowColor(rainbows[i]);
     }
-    
-    function rand1to255(){
-        return Math.ceil(Math.random()*255);
+}, 150)
+
+function lights(){
+    for(let i=3; i<20; i+=1){
+        treeContainer.innerHTML += '<br>';
+        for(let j=0; j<=i; j++){
+
+                treeContainer.innerHTML += '<span class="rainbow"> O </span>';
+            
+        }
     }
-}, 200)
+}
+
+function rainbowColor(element){
+    element.style['color'] = `rgb(${rand1to255()}, ${rand1to255()}, ${rand1to255()})`;
+    element.style['text-shadow'] = `rgb(${rand1to255()}, ${rand1to255()}, ${rand1to255()}) 0 0 5px`;
+}
+
+function rand1to255(){
+    return Math.ceil(Math.random()*255);
+}
